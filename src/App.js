803f5c81c6d7f970d86303710706e78395b7ea92
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-07-07 07:48:43 -0700

    added dependencies and boilerplate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import MainApp from "./components/MainApp";
 
 function App() {
   return (
     <div className="App">
-      <h2>App</h2>
+      <MainApp />
     </div>
   );
 }
