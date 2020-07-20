ec59898398670a6b107cec709a506218c3527351
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-07-20 12:07:04 -0700

    added example imgs

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,25 +3,52 @@ import image from "../imgs/jarritos.jpg";
 
 const Content = () => {
   return (
-    <section className="Content">
+    <div>
+      <section className="Content">
+        {/**
       <h1>daily specials</h1>
-      <div className="special-wrapper">
-        <div className="special-img-wrapper">
-          <img src={image} alt="" />
+      */}
+        <h1>daily specials</h1>
+
+        <div className="special-wrapper">
+          <div className="special-img-wrapper">
+            <img src={image} alt="" />
+          </div>
+          <div className="special-description">
+            <h2>special's name</h2>
+            <span>$5.99</span>
+            <p>specials description </p>
+          </div>
         </div>
-        <div className="special-description">
-          <h2>special's name</h2>
-          <span>$5.99</span>
-          <p>specials description </p>
+      </section>
+
+      <div className="med-content">
+        <div className="med-content-inner">
+          <h2>in a rush?</h2>
+          <p>give us a call to prepare your oder.</p>
+          <a href="tel:+702-343-2343">call</a>
         </div>
       </div>
 
-      <div className="med-content">
-        <h2>in a rush?</h2>
-        <p>give us a call to prepare your oder.</p>
-        <a href="tel:+702-343-2343">call</a>
+      <div className="lower-content">
+        <h1>new items</h1>
+
+        <div className="lower-content-wrapper">
+          <div className="menu-description">
+            <h2>special's name</h2>
+            <span>$5.99</span>
+            <p>specials description </p>
+          </div>
+
+          <div className="menu-imgs">
+            <img src={image} alt="" />
+            <img src={image} alt="" />
+            <img src={image} alt="" />
+            <img src={image} alt="" />
+          </div>
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
 
