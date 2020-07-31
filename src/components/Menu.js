53ecb2b0cb97136678ca195c78c851fc4c736dfa
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-07-30 18:46:43 -0700

    adding menu section

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 import MobileNavar from "./MobileNavbar";
+import MenuHeader from "./MenuHeader";
 
 const Menu = () => {
   return (
     <div>
       <Navbar />
       <MobileNavar />
+      <MenuHeader />
       <h2>Menu</h2>
     </div>
   );
